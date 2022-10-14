a5295eca717e389f6978a52f098d7c6c0309bb0b
SrPhG <cveras2021@elam.co.uk>
2022-10-14 14:55:17 +0100

    v1.8.2
    more animation stuff

--- VR/Assets/Scripts/AnimatedAssets.cs
+++ VR/Assets/Scripts/AnimatedAssets.cs
@@ -6,50 +6,37 @@ using UnityEngine.XR;
 public class AnimatedAssets : MonoBehaviour
 {
 
-    public Transform person;
-    public Transform myStartPoint;
-    public Transform myEndPoint;
+    Transform person;
     public Transform[] points;
     public int nextPoint = 1;
 
     public float speed;
 
-    bool isReversing = false;
-
     // Start is called before the first frame update
     void Start()
     {
+        person = transform;
         person.position = points[0].position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //person.position = Vector3.MoveTowards(person.position, myEndPoint.position, speed);
-
-
-
-        if (isReversing == false)
+        if (person.position != points[nextPoint].position)
         {
             person.position = Vector3.MoveTowards(person.position, points[nextPoint].position, speed);
 
             if (person.position == points[nextPoint].position)
             {
-                if (points.Length > nextPoint-1)
+                if (points.Length > nextPoint+1)
                 {
                     nextPoint++;
                 }
-                //isReversing = true;
             }
         }
         else
         {
-            person.position = Vector3.MoveTowards(person.position, points[0].position, speed);
-
-            if (person.position == points[0].position)
-            {
-                isReversing = false;
-            }
+            Destroy(person.gameObject);
         }
     }
 }
